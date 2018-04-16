4173b1f9f5788eaf96728c883c910dfd1dc496e5
Marshall126 <rfmaddenh@gmail.com>
2018-04-15 21:25:21 -0500

    worked on content and bibliography

--- resources/js/slider.js
+++ resources/js/slider.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //slider stuff
     $('.content').slick({
       dots:false,
       slidesToScroll:1,
@@ -8,5 +9,42 @@ $(document).ready(function(){
       infinite:true, 
       autoplay:true,
     });
+    //animating bibliography
+    let big = false;
+    $('.bib').click(function(){
+      if(big==false){
+        $('.bib ol').css({
+          display:'inline-block'
+        });
+        if($('.test').css('display')=='flex'){
+          $('.bib').animate({
+            width: '80%',
+            height: '600px'
+          }, 300);
+        }else if($('.test').css('display')=='block'){
+          $('.bib').animate({
+            width: '80%',
+            height: '700px'
+          }, 300);
+        }else if($('.test').css('display')=='inline'){
+          $('.bib').animate({
+            width: '80%',
+            height: '800px'
+          }, 300);
+        }else{
+          $('.bib').animate({
+            width: '80%',
+            height: '500px'
+          }, 300);
+        }
+        big = true;
+      }else{
+        $('.bib').animate({width: '400px', height: '50px'}, 300);
+        $('.bib ol').css({
+          display:'none'
+        })
+        big = false;
+      }
+    })
 });
 
